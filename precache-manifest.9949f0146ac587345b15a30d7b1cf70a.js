270d7e7113782ce7223a4bb9577de118a54795b2
pallavi <pathak.pallavi93@gmail.com>
2019-11-30 17:59:23 +0530

    Updates

---
+++ precache-manifest.9949f0146ac587345b15a30d7b1cf70a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e72daa144bba52aa75dad78a88ff04",
+    "url": "/news-search-widget/index.html"
+  },
+  {
+    "revision": "9fac45ff1e5818bc41e8",
+    "url": "/news-search-widget/static/css/main.ac198351.chunk.css"
+  },
+  {
+    "revision": "f9415ed4b99191df5f08",
+    "url": "/news-search-widget/static/js/2.221a5a7a.chunk.js"
+  },
+  {
+    "revision": "9fac45ff1e5818bc41e8",
+    "url": "/news-search-widget/static/js/main.360fd7cb.chunk.js"
+  },
+  {
+    "revision": "f8a64d383cd1728636c1",
+    "url": "/news-search-widget/static/js/runtime-main.beb163da.js"
+  }
+]);
